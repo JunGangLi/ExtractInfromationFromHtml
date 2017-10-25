5fc4b9c1b74b253511728add9b299fed5a23345b
li <1301141753>
2017-10-25 09:44:15 +0800

    1025

--- EncodeingHtml/GetInfo_fromHtml/Form1.Designer.cs
+++ EncodeingHtml/GetInfo_fromHtml/Form1.Designer.cs
@@ -184,7 +184,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(526, 262);
+            this.ClientSize = new System.Drawing.Size(526, 268);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
--- EncodeingHtml/GetInfo_fromHtml/Form1.cs
+++ EncodeingHtml/GetInfo_fromHtml/Form1.cs
@@ -193,6 +193,7 @@ namespace GetInfo_fromHtml
             }
             else
                 dateTimeStart.Enabled = true;
+            
         }
 
         private void getHistoryAirData(object opath)
@@ -294,9 +295,7 @@ namespace GetInfo_fromHtml
                         dateTimeStart.Value = dateTimeEnd.Value;
                     }
                     Action<DateTime, DateTime, string> getdata02 = new Action<DateTime, DateTime, string>(getDay);
-                    getdata02.BeginInvoke(dateTimeStart.Value, dateTimeEnd.Value, ofd.FileName, recall, null);
-                    //DateTime time = new DateTime(2012, 10, 8, 13, 0, 0);
-                    //getDay(time, time, ofd.FileName);
+                    getdata02.BeginInvoke(dateTimeStart.Value, dateTimeEnd.Value, ofd.FileName, recall, null);                  
                 }
             }
         }
@@ -304,13 +303,13 @@ namespace GetInfo_fromHtml
         private void getDay(DateTime start, DateTime end,string pathfile)
         {
             int pageNo = 1;
-
             using (StreamWriter sw = new StreamWriter(pathfile, true))
-            {               
+            {
+                
                 List<string[]> dayData = null;
                 int uablecount = 0;
                 do
-                {
+                {                    
                     dayData = null;
                     dayData = EnvironmentalData.getDayData(pageNo, "", start, end);
                     if (dayData.Count==0)
@@ -327,11 +326,10 @@ namespace GetInfo_fromHtml
                         sw.WriteLine();          
                     }                            
                     pageNo++;
-                } while (uablecount <= 2);                                
+                    GC.Collect();
+                } while (uablecount <= 5);                                
                 sw.Close();
             }
-            
-           
         }
     }
 }
